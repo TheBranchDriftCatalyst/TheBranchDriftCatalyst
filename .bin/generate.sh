bfcc8e696b630b545e6c8b789be4d8f7cb80f18d
DJ Daniels <djdanielsh@gmail.com>
2025-09-30 17:52:14 -0700

    misc updates preparing for catalyst core repo integration

---
+++ .bin/generate.sh
@@ -0,0 +1,14 @@
+TODO:
+
+This script will go through my local ./workspace/* 
+directory and collect the data and badges in order 
+to construct a sexy synthwave style markdown github home page.
+
+- To include 
+  - waka time graphs
+  - github stats
+  - repo badges (issues, prs, stars, etc)
+  
+  Maybe use open AI to generate a summary of my work and add it here
+
+  maybe this is a go binary we just shove into here
